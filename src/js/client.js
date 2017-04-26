419baf6c7d2e7650fbc8bdb98d3e13b97457228d
cjewell47 <charles.jewell47@gmail.com>
2017-04-26 19:27:03 +0100

    ANIMATION NOW WORKING

--- src/js/client.js
+++ src/js/client.js
@@ -4,18 +4,14 @@ $(() => {
 
   $('.index-beer').on('mouseenter', function() {
     // $(this).find('.index-text').css({display: 'inline-block'});
-    $(this).find('.index-text').animate({'display': 'inline-block'});
+    $(this).find('.index-text').delay(120).fadeIn();
 
   });
 
   $('.index-beer').on('mouseleave', function() {
-    $(this).find('.index-text').css({display: 'none'});
-
+    // $(this).find('.index-text').css({display: 'none'});
+    $(this).find('.index-text').delay(120).fadeOut();
   });
 
 
-
 });
-
-
-.fadeIn('slow', function());
