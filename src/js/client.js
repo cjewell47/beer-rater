1e3fa44d0b0a415f739f2f1f4083e9a07d6f86fd
cjewell47 <charles.jewell47@gmail.com>
2017-04-26 19:14:44 +0100

    index page styling

--- src/js/client.js
+++ src/js/client.js
@@ -1,19 +1,21 @@
 console.log('hello world');
 
-// const API_KEY = '46334ee126026181b04f8f030e8220ce';
+$(() => {
 
-// 'https://api.brewerydb.com/v2/search?q=a&type=beer&key=46334ee126026181b04f8f030e8220ce&format=json'
+  $('.index-beer').on('mouseenter', function() {
+    // $(this).find('.index-text').css({display: 'inline-block'});
+    $(this).find('.index-text').animate({'display': 'inline-block'});
 
-// $(init);
-//
-// function init() {
-//   getBeers();
-// }
-//
-// function getBeers() {
-//   $
-//     .get(`http://api.brewerydb.com/v2/?key=${API_KEY}`)
-//     .done(data => {
-//       console.log(data);
-//     });
-// }
+  });
+
+  $('.index-beer').on('mouseleave', function() {
+    $(this).find('.index-text').css({display: 'none'});
+
+  });
+
+
+
+});
+
+
+.fadeIn('slow', function());
